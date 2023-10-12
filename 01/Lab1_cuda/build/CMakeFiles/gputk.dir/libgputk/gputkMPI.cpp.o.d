c75def82bc580e5118dfe53a2ec118e590569c5b
Patrick Yi <pjyi2147@gmail.com>
2023-10-12 22:31:38 +0900

    Delete build and cmake related files as they collide on cluster

--- 01/Lab1_cuda/build/CMakeFiles/gputk.dir/libgputk/gputkMPI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gputk.dir/libgputk/gputkMPI.cpp.o: \
- /home/jeongseopyi/CSED490C/lab/01/Lab1_cuda/libgputk/gputkMPI.cpp \
- /usr/include/stdc-predef.h
--- 02/Lab2_cuda/build/CMakeFiles/gputk.dir/libgputk/gputkMPI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gputk.dir/libgputk/gputkMPI.cpp.o: \
- /home/patrick/school/postech-F23/CSED490C/CSED490C-lab/02/Lab2_cuda/libgputk/gputkMPI.cpp \
- /usr/include/stdc-predef.h
