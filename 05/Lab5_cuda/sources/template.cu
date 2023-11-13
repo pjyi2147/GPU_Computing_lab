e359a02cd0520e9de769624447c43f9253453d34
Patrick Yi <pjyi2147@gmail.com>
2023-11-13 23:41:57 +0900

    LAB 5

---
+++ 05/Lab5_cuda/sources/template.cu
@@ -0,0 +1,69 @@
+#include <gputk.h>
+
+#define NUM_BINS 4096
+
+#define CUDA_CHECK(ans)                                                   \
+  { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code),
+            file, line);
+    if (abort)
+      exit(code);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  gpuTKArg_t args;
+  int inputLength;
+  unsigned int *hostInput;
+  unsigned int *hostBins;
+  unsigned int *deviceInput;
+  unsigned int *deviceBins;
+
+  args = gpuTKArg_read(argc, argv);
+
+  gpuTKTime_start(Generic, "Importing data and creating memory on host");
+  hostInput = (unsigned int *)gpuTKImport(gpuTKArg_getInputFile(args, 0),
+                                       &inputLength, "Integer");
+  hostBins = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
+  gpuTKTime_stop(Generic, "Importing data and creating memory on host");
+
+  gpuTKLog(TRACE, "The input length is ", inputLength);
+  gpuTKLog(TRACE, "The number of bins is ", NUM_BINS);
+
+  gpuTKTime_start(GPU, "Allocating GPU memory.");
+  //@@ Allocate GPU memory here
+  CUDA_CHECK(cudaDeviceSynchronize());
+  gpuTKTime_stop(GPU, "Allocating GPU memory.");
+
+  gpuTKTime_start(GPU, "Copying input memory to the GPU.");
+  //@@ Copy memory to the GPU here
+  CUDA_CHECK(cudaDeviceSynchronize());
+  gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
+
+  // Launch kernel
+  // ----------------------------------------------------------
+  gpuTKLog(TRACE, "Launching kernel");
+  gpuTKTime_start(Compute, "Performing CUDA computation");
+  //@@ Perform kernel computation here
+  gpuTKTime_stop(Compute, "Performing CUDA computation");
+
+  gpuTKTime_start(Copy, "Copying output memory to the CPU");
+  //@@ Copy the GPU memory back to the CPU here
+  CUDA_CHECK(cudaDeviceSynchronize());
+  gpuTKTime_stop(Copy, "Copying output memory to the CPU");
+
+  gpuTKTime_start(GPU, "Freeing GPU Memory");
+  //@@ Free the GPU memory here
+  gpuTKTime_stop(GPU, "Freeing GPU Memory");
+
+  // Verify correctness
+  // -----------------------------------------------------
+  gpuTKSolution(args, hostBins, NUM_BINS);
+
+  free(hostBins);
+  free(hostInput);
+  return 0;
+}
